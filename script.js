f761988714589aba79f6d73f0cfb09837e8408b3
shivananandkumar058-creator <shivananandkumar058@gmail.com>
2026-09-19 23:44:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,121 @@
+const accountId = 12344
+const accountEmail = "shiva@123"
+const accountpassword = "12345sk"
+let accountstate;
+//console.log(accountId);
+//console.log(accountEmail)
+//console.log(accountpassword)
+//console.table([accountId, accountEmail, accountpassword, accountstate])
+
+//console.log(typeof "shiva")
+//console.log(typeof 12344)
+//let valueInNumber = Number(score)
+  //console.log(typeof valueInNumber)
+
+  let isLoggedIn = 1
+  let booleanIsLoggedIn = Boolean(isLoggedIn)
+  //console.log(booleanIsLoggedIn)
+
+  let value = 4
+  let negvalue = -value
+  //console.log(negvalue)
+
+  let str1 = "shiva"
+  let str2 = "kumar"
+  let str3 = str1+str2
+  console.log(str3)
+console.log(1+2)
+console.log(1-2)
+console.log(1/2)
+  console.log("1"+2)
+  console.log(1+"2")
+  console.log("1"+1+2)
+  console.log(1+2+"2")
+
+  const heross = ["shaktiman","prabhash","heros"]
+   let myObject ={
+      name: "shiva",
+      age: 21,
+      class: "b tech",
+   }
+  console.log(myObject)
+  console.log(heross)
+     let gamecounter = 100
+      ++gamecounter
+      //console.log(gamecounter)
+
+      ///console.log(2>1)
+      //console.log(2<1)
+      //console.log(2==1)
+      //console.log("2">1)
+    //  console.log("02">1)
+
+      //console.log(null==0);
+      //console.log(null<0);
+      //console.log(null>=);
+
+      const heros = ["saktiman","nagraj","doga"]
+        let myObj ={
+          name: "shiva",
+          age: 21,
+        }
+        console.log(myObj)
+        console.log(heros)
+
+          const myFunction = function(){
+            console.log("hello world");
+          }
+
+          let myYouTubeName = "shiva"
+          let anotherName = myYouTubeName
+            anotherName = "chai aur code"
+
+          console.log(myYouTubeName)
+          console.log(anotherName)
+
+          let userone ={
+            email:"user@123",
+            upi:"user@12",
+          }
+        console.log(userone)
+
+        const name = "shiva"
+        const repocount = 50
+        console.log(name + repocount + "value")
+          console.log("hello my name is ${name} and any repocount is ${repocount}")
+
+          const gameName = new String('thitishhc')
+           console.log(gameName[0])
+           console.log(gameName.__proto__)
+           console.log(gameName.length) 
+           console.log(gameName.charAt('2'))
+           console.log(gameName.indexOf("t"))
+
+           const anotherString = gameName.slice(-8,4)
+           console.log(anotherString)
+
+           const newStringone = "  shiva"
+           console.log(newStringone)
+           console.log(newStringone.trim())
+
+           const score = 400
+           console.log(score)
+           const balance = new Number(100)
+           console.log(balance)
+           console.log(balance.toString())
+           console.log(balance.toString().length)
+
+           console.log(balance.toFixed(2))
+
+           const otherNumber = 123.8966
+             console.log(otherNumber.toPrecision(3))
+
+             const hundreds = 10000000
+             console.log(hundreds.toLocaleString('en-IN'))
+
+             console.log(maths);
+             console.log(maths.abs(-4))
+             console.log(maths.round(4.6))
+
+
+
